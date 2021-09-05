b4595b32d76c0d2c46ca87aa163232bdd262dd0f
kamalharsh14 <singhharsh998@gmail.com>
2021-09-05 22:05:09 +0530

    Metacharacters

---
+++ src/Regex/metacharacters.java
@@ -0,0 +1,13 @@
+package Regex;
+
+import java.util.*;
+import java.util.regex.*;
+
+public class metacharacters {
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        in.close();
+        System.out.println(Pattern.matches("d*", "123"));
+    }
+}
