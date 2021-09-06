8860cfdb270ed4fdf745fe7b21b50f3234520ae9
kamalharsh14 <singhharsh998@gmail.com>
2021-09-06 09:59:28 +0530

    Assignments

--- src/Assignments/ques2.java
+++ src/Assignments/ques2.java
@@ -1,6 +1,6 @@
-package FINAL450;
+package Assignments;
 import java.util.*;
-public class q2 {
+public class ques2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
