218d6c72959247f09026b31a2f504e2cf8276863
kamalharsh14 <singhharsh998@gmail.com>
2021-09-10 07:23:39 +0530

    Random

---
+++ src/Learnings/random3.java
@@ -0,0 +1,13 @@
+package Learnings;
+
+public class random3{
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
+        System.out.println("Harsh Kamal Singh");
+        System.out.println("Harsh Kamal  is a good ");
+        System.out.println("Harsh Kamal  is a boy");
+        System.out.println();
+        System.out.println();
+    }
+}
