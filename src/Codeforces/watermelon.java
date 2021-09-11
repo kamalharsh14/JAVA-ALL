609f000181915be109227aa9c94850590a69131b
kamalharsh14 <singhharsh998@gmail.com>
2021-09-11 15:55:33 +0530

    Watermelon

---
+++ src/Codeforces/watermelon.java
@@ -0,0 +1,17 @@
+package Codeforces;
+
+import java.util.Scanner;
+
+public class watermelon {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int w = in.nextInt();
+        if(w %2 == 0 && w > 2){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+        in.close();
+    }
+}
