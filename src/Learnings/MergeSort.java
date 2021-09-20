863a851f21ba907c2aeb61b28410d542822127bb
kamalharsh14 <singhharsh998@gmail.com>
2021-09-20 14:24:55 +0530

    Merge Sort

--- src/Learnings/MergeSort.java
+++ src/Learnings/MergeSort.java
@@ -57,8 +57,15 @@ public class MergeSort{
     }
 
     void print(int arr[]){
-        for(int i = 0 ; i < arr.length; i ++){
-            System.out.print(arr[i]+" ");
-        }
+            System.out.print("[");
+            for(int i = 0 ; i < arr.length; i ++){
+            if(i < arr.length -1){
+                System.out.print(arr[i]+",");
+            }
+            else{
+                System.out.print(arr[i]+"");
+            }
+            }
+            System.out.print("]");
     }
 }
