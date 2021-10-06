c596680f5853473147dce7c4e01cacc0a19b5b05
kamalharsh14 <singhharsh998@gmail.com>
2021-10-06 16:00:49 +0530

    CodeChef

--- src/Codechef/ANDSUBAR.java
+++ src/Codechef/ANDSUBAR.java
@@ -5,23 +5,8 @@ public class ANDSUBAR {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
         while(t-- > 0){
-            int odd = 0;
-            int even = 0;
             int n = in.nextInt();
-            for(int i = 1 ; i <= n ; i ++){
-                if(i % 2 == 0){
-                    odd++;
-                }
-                else{
-                    even++;
-                }
-            }
-            if(odd == even){
-                System.out.println(2*odd);
-            }
-            else{
-                System.out.println(2*Math.min(odd, even));
-            }
+            
         }
         in.close();
     }
