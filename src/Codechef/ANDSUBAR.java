4cabd9979479da60438b0bb7980fa66d1801b3f3
kamalharsh14 <singhharsh998@gmail.com>
2021-10-07 08:03:17 +0530

    CodeChef

--- src/Codechef/ANDSUBAR.java
+++ src/Codechef/ANDSUBAR.java
@@ -6,7 +6,15 @@ public class ANDSUBAR {
         int t = in.nextInt();
         while(t-- > 0){
             int n = in.nextInt();
-            
+            int a = n;
+            int count = 0;
+            while(a > 0){
+                count = count + 1;
+                a = a>>1;
+            }
+            int ans1 = n -  (int)Math.pow(2, count-1) + 1;
+            int ans2 = (int)Math.pow(2, count-1) - (int)Math.pow(2, count-2);
+            System.out.println(Math.max(ans1, ans2));
         }
         in.close();
     }
