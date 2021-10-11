edd4b7baf559e412a383e55970a9fe3544c7cd6d
kamalharsh14 <singhharsh998@gmail.com>
2021-10-11 11:43:07 +0530

    CodeChef

--- src/Codechef/DIGITREM.java
+++ src/Codechef/DIGITREM.java
@@ -8,6 +8,35 @@ public class DIGITREM {
             int N = in.nextInt();
             int D = in.nextInt();
             int counter = 0;
+            int n = N;
+            int c = 0;
+            int d = 0;
+            while(n > 0){
+                d = n%10;
+                n = n/10;
+                c++;
+
+                if(d == D){
+                    n = (int) (n*Math.pow(10, c) + (d+1)*Math.pow(10, c-1));
+                    counter = n - N;
+                    c = 0;
+                }
+                
+            }
+            System.out.println(counter);
+        }
+        in.close();
+    }
+
+
+    //Time Limit Exceeded
+    /* public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while(t-- > 0){
+            int N = in.nextInt();
+            int D = in.nextInt();
+            int counter = 0;
             while(N > 0){
                 boolean check = digitscheck(N,D);
                 if(check == true){
@@ -33,5 +62,5 @@ public class DIGITREM {
             n = n/10;
         }
         return t;
-    }
+    } */
 }
