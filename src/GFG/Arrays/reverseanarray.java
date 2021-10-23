948bbaf5afd3149c8eac1113db21dd0cc98d68e1
kamalharsh14 <singhharsh998@gmail.com>
2021-10-23 09:14:55 +0530

    Arrays

--- src/GFG/Arrays/reverseanarray.java
+++ src/GFG/Arrays/reverseanarray.java
@@ -1,5 +1,29 @@
 package GFG.Arrays;
-
+import java.util.*;
 public class reverseanarray {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n =  in.nextInt();
+        int a[] = new int[n];
+        
+        for(int i = 0 ; i < n ; i ++){
+            a[i] = in.nextInt();
+        }
+
+        int start = 0;
+        int end = n-1;
+        
+        while(start < end){
+            int t = a[start];
+            a[start] = a[end];
+            a[end] = t;
+            start++;
+            end--;
+        }
+        
+        for(int i = 0 ; i < n ; i ++){
+            System.out.print(a[i]+" ");
+        }
+        in.close();
+    }
 }
