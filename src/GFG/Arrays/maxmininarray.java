ce7d5a511d4f18b35e6ce2606f5250fc4ae58e18
kamalharsh14 <singhharsh998@gmail.com>
2021-10-23 09:20:15 +0530

    Arrays

---
+++ src/GFG/Arrays/maxmininarray.java
@@ -0,0 +1,30 @@
+package GFG.Arrays;
+import java.util.*;
+public class maxmininarray {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a[] = new int[n];
+        
+        for(int i = 0 ; i < n ; i ++){
+            a[i] = in.nextInt();
+        }
+
+        int max = -99999999; 
+        int min = 99999999;
+
+        for(int i = 0 ; i < n ; i++){
+            if(min > a[i]){
+                min = a[i];
+            }
+            if(max < a[i]){
+                max = a[i];
+            }
+        }
+
+        System.out.println("Min = "+min);
+        System.out.println("Max = "+max);
+
+        in.close();
+    }
+}
