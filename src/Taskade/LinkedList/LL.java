27221ab41a6c32d4366aff46dd0ebceebac1ebee
Harsh Kamal Singh <singhharsh998@gmail.com>
2021-12-09 13:26:00 +0530

    Linked List

---
+++ src/Taskade/LinkedList/LL.java
@@ -0,0 +1,44 @@
+package Taskade.LinkedList;
+
+public class LL {
+
+    public class Node{
+        public Node next;
+        
+        int data;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head = null;
+
+    void insert(int data){
+        Node insert=  new Node(data);
+        if(head == null){
+            head = insert;
+        }
+        else{
+            Node temp = head;
+            while(temp.next != null){
+                temp = temp.next;
+            }
+            temp.next = insert;
+        }
+    }
+
+    Node gethead(){
+        return head;
+    }
+
+    void display(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data+" -> ");
+            temp = temp.next;
+        }
+        System.out.println("Null");
+    }
+}
